fe15c5518cfe651cff326a9d8baa2f1a00da1490
Ankan Mukherjee <ankanmukherjee635@gmail.com>
2025-05-09 22:44:12 +0530

    finally done

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import logger from "./logger.js";
+import morgan from "morgan";
 
 const app = express();
 const port = 3001;
+const morganFormat = ":method :url :status :response-time ms";
+
 
 //we want to add data in the array
 app.use(express.json())
@@ -9,6 +13,22 @@ app.use(express.json())
 // when you take the data use post you can use get as well
 let teaData=[]
 let newId=1
+//added use from lec98 to create the advancelogger 
+app.use(
+  morgan(morganFormat, {
+    stream: {//stream o0f data theke ja ja dorkar to show i can display 
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],//this is get ,put ,post oi method gulo dekhabe 
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],// this will show /teas /abcd etc 
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 app.post('/teas',(req,res)=>{
     const{name,price}=req.body;
     const newTea={id:newId++,name,price}
